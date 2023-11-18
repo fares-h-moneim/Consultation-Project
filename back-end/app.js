818e3f5faa0302486bacbf901a131bb33423edfb
Yehia Tarek Ragab <147417980+yehiar63@users.noreply.github.com>
2023-11-18 23:53:11 +0200

    fixes

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import {mongoose} from 'mongoose';
 
-const dbUrl = 'mongodb://localhost:27017/admin';
+const dbUrl = 'mongodb://localhost:27017/test';
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.log(err));
