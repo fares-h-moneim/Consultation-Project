419d8149644a8f50f0baeba8b909e5b749bc803d
Fares Hesham <fares.h.moneim@gmail.com>
2023-11-19 12:42:42 +0200

    Database Created missing tables

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,36 @@
-import express from 'express';
-import {mongoose} from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
 
-const dbUrl = 'mongodb://localhost:27017/test';
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log(err));
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.2", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB Connected Successfully");
+    } catch (err) {
+        console.error(err);
+    }
+};
 
+connectDB();
+
+const UserSchema = mongoose.Schema({
+    name: String,
+    age: Number
+})
+
+const UserModel = mongoose.model("users", UserSchema);
+
+app.get("/getUsers", (req, res) => {
+    res.json(UserModel.find({}).then(function (users) {
+        res.json(users);
+    })).catch((err) => res.status(400).json({
+        err
+    }));
+});
+
+app.listen(3000, () => {
+    console.log("On Port 3000");
+})
