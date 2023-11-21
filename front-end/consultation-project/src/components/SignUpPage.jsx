bad18b992cb886920090e2a55c2f341dcd83b88b
Fares Hesham <fares.h.moneim@gmail.com>
2023-11-21 20:08:41 +0200

    Added form validations to Signup page

--- front-end/consultation-project/src/components/SignUpPage.jsx
+++ front-end/consultation-project/src/components/SignUpPage.jsx
@@ -3,7 +3,7 @@ import SignUp from "./SignUp";
 
 export default function SignUpPage() {
     return (
-        <div className="container-fluid px-0 content" style={{ backgroundColor: "red", height: "92vh" }}>
+        <div className="container-fluid px-0 content" style={{ backgroundColor: "red", height: "auto" }}>
             <div className="row align-items-center">
                 <MascotImage />
                 <SignUp />
