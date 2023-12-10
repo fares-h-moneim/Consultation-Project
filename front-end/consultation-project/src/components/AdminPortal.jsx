4e3af5e058d85e0a2b8d57b1848f558e6005e298
Fares Hesham <fares.h.moneim@gmail.com>
2023-12-10 15:58:17 +0200

    Admin portal frontend done

--- front-end/consultation-project/src/components/AdminPortal.jsx
+++ front-end/consultation-project/src/components/AdminPortal.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import MascotImage from "./MascotImage";
 import ButtonImage from "./ButtonImage";
-import image from "../assets/Ahly.png";
+import userPlus from "../assets/user-plus-solid.svg";
+import trash from "../assets/trash-solid.svg";
 
 export default function AdminPortal() {
     return (
         <div className="container-fluid px-0 content" style={{ backgroundColor: "red", height: "92vh" }}>
             <div className="row align-items-center">
                 <MascotImage />
-                <div className="col mt-3 mb-3 ml-5 mr-5">
-                    <ButtonImage text="View Users Requests" image={image}></ButtonImage>
-                </div>
-                <div className="col mt-3 mb-3 ml-5 mr-5">
-                    <ButtonImage text="Remove Users" image={image}></ButtonImage>
+                <div className="col">
+                    <div className="row align-items-center">
+                        <div className="col text-center">
+                            {/*TODO: Change the text to the admin name*/}
+                            <p className="display-3 text-white" style={{ fontWeight: "bolder" }}>Hello Admin!</p>
+                        </div>
+                    </div>
+                    <div className="row align-items-center">
+                        <div className="col mt-3 mb-3 ml-5 mr-5">
+                            <ButtonImage text="View Users Requests" image={userPlus}></ButtonImage>
+                        </div>
+                        <div className="col mt-3 mb-3 ml-5 mr-5">
+                            <ButtonImage text="Remove Users" image={trash}></ButtonImage>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- front-end/consultation-project/src/components/ButtonImage.jsx
+++ front-end/consultation-project/src/components/ButtonImage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function ButtonImage({ text, image }) {
     return (
-        <button type="button" className="btn btn-danger" style={{ width: "75%", height: "auto", backgroundColor: "white", color: "black" }}>
+        <button type="button" className="btn btn-danger" style={{ width: "75%", height: "250px", backgroundColor: "white", color: "black" }}>
             <img src={image} alt="stadium Image" style={{ width: "50%", height: "auto" }} />
-            <h5 style={{ marginLeft: "10px" }}>{text}</h5>
+            <h5 style={{ marginLeft: "10px", paddingTop: "25px" }}>{text}</h5>
         </button>
     );
 }
