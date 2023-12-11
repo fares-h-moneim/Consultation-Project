b27442152991c923fbf8e38c4bcb9629ddb50f61
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2023-12-11 21:28:00 +0200

    added endpoint

--- back-end/routes/match.js
+++ back-end/routes/match.js
@@ -3,6 +3,7 @@ import { getMatches, addMatch, bookMatch } from "../controller/match.js";
 import { requireFan, requireManager } from "../controller/passport-setup.js";
 const matchRouter = express.Router();
 matchRouter.get("/get-matches", getMatches);
+matchRouter.get("/get-match/:matchId", getMatchById);
 matchRouter.post("/add-match", requireManager, addMatch);
 matchRouter.post("/book-match/:matchId/:userId", requireFan, bookMatch);
 export default matchRouter;
