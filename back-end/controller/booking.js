3b8e6afd5976e0af4407d8148aa0eced579dd84a
Fares Hesham <fares.h.moneim@gmail.com>
2023-12-15 13:31:42 +0200

    fixed bookings endpoint

--- back-end/controller/booking.js
+++ back-end/controller/booking.js
@@ -81,7 +81,7 @@ const deleteBooking = async (req, res) => {
     }
 };
 
-const getUserBookingsforMatch = async (req, res) => {
+const getUserBookings = async (req, res) => {
     try {
         const token = req.header('Authorization');
         if (!token) {
